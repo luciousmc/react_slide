986f8cd84f12d7f2e702f4f68c2f44c3c5aa0975
Marlon Clay <luciousmc@icloud.com>
2020-12-07 12:54:27 -0800

    adds test images start slider component

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Slider from './slider';
+
+const App = () => {
+  return(
+    <>
+      <h1>React slide</h1>
+      <hr/>
+      <Slider>
+        <div>how</div>
+        <div>are</div>
+        <div>you</div>
+        <div>doing</div>
+      </Slider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/slider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Slider = ({ children }) => {
+  return (
+    <div className="slide-container">
+      
+    </div>
+  )
+}
+
+export default Slider;
