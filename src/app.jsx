cba8632c4bb08163334dedbc7eefcabfb883317e
Marlon Clay <luciousmc@icloud.com>
2020-12-07 13:52:55 -0800

    added styled components

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Slider from './slider';
 
+
 const App = () => {
   return(
     <>
       <h1>React slide</h1>
       <hr/>
       <Slider>
-        <div>how</div>
-        <div>are</div>
-        <div>you</div>
-        <div>doing</div>
+        <img src="../images/image1.jpg" alt=""/>
+        <img src="../images/image2.jpg" alt=""/>
+        <img src="../images/image3.jpg" alt=""/>
+        <img src="../images/image4.jpg" alt=""/>
+        <img src="../images/image5.jpg" alt=""/>
       </Slider>
     </>
   );
--- src/slider.jsx
+++ src/slider.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Slider = ({ children }) => {
   return (
     <div className="slide-container">
-      
+      {
+        children.map((child,i) => {
+          return (
+            <div key={i} id={`slide-${i}`} className="slide">{child}</div>
+          );
+        })
+      }
     </div>
   )
 }
