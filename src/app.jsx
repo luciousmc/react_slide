becbf3b56c0a05a9596ff835d7d6e9d75f9ecbfd
Marlon Clay <luciousmc@icloud.com>
2020-12-09 21:35:32 -0800

    styled components

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Slider from './slider';
 
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 3rem;
+  text-shadow: 1px 1px 5px green;
+  margin: 1.5rem auto;
+`
 
 const App = () => {
   return(
     <>
-      <h1>React slide</h1>
-      <hr/>
+      <Title>React slide</Title>
       <Slider>
         <img src="../images/image1.jpg" alt=""/>
         <img src="../images/image2.jpg" alt=""/>
